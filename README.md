c087766f60b7402e98f5f54e6ff747fd448a9a70
jasmaa <jasonmaa3955@gmail.com>
2019-02-02 19:45:11 -0500

    Add file reading

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # bf-go
 
 Brainfuck interpreter in Go
+
+# Usage
+  - `go run <program path>`
+
+# Acknowledgements
+  - Samples retrieved from Daniel Cristofani's [site](http://www.hevanet.com/cristofd/brainfuck/)
--- bf.go
+++ bf.go
@@ -1,107 +1,138 @@
+// Interprets brainfuck programs
 package main
 
 import (
 	"fmt"
 	"bufio"
 	"os"
+	"io/ioutil"
 )
 	
 var program []byte
 var pc int
 var memory []byte
 var dc int
-
 var inp []byte
 
-func step(){
+// Load program
+func load(fname string){
+
+	data, err := ioutil.ReadFile(fname)
 	
-	switch program[pc] {
-		case '>':
-			dc++
-		case '<':
-			dc--
-		case '+':
-			memory[dc]++
-		case '-':
-			memory[dc]--
-		case '.':
-			fmt.Printf("%c", memory[dc])
-		case ',':
-			reader := bufio.NewReader(os.Stdin)
-			
-			_, err := reader.Read(inp)
-			
-			if err != nil {
-				fmt.Println("Error reading input")
-				return
-			}
-			
-			memory[dc] = inp[0]
-			
-		case '[':
-			
-			if memory[dc] == 0 {
-				// search for matching closing
-				encounters := 1
-				 for {
-					pc++
-					if program[pc] == '[' {
-						encounters++
-					} else if program[pc] == ']' {
-						encounters--
-						if encounters == 0 {
-							break
-						}
-					}
-				 }
-			}
-			
-		case ']':
-			
-			if memory[dc] != 0 {
-				// search for matching opening
-				encounters := 1
-				 for {
-					pc--
-					if program[pc] == ']' {
-						encounters++
-					} else if program[pc] == '[' {
-						encounters--
-						if encounters == 0 {
-							break
-						}
-					}
-				 }
-			}
-			
-		default:
-			fmt.Println("error")
+	if err != nil {
+		fmt.Println("ERROR: Could not read in file")
+		return
 	}
 	
-	// debug
-	//fmt.Println("PC:", pc)
-	//fmt.Printf("%q\n", program[pc])
-	//fmt.Println("Data", memory[:10])
-	//fmt.Println("===")
+	// Initialize
+	program = data
+	pc = 0
+	memory = make([]byte, 30000)
+	dc = 0
+	inp = make([]byte, 1)
+}
+
+// Run program
+func run(){
+
+	for ; pc < len(program); {
 	
-	// Increment program counter
-	pc++
+		// step
+		switch program[pc] {
+			case '>':
+				dc++
+				if dc >= len(memory) {
+					fmt.Println("ERROR: Data pointer out-of-bounds")
+					return
+				}
+				
+			case '<':
+				dc--
+				if dc < 0 {
+					fmt.Println("ERROR: Data pointer out-of-bounds")
+					return
+				}
+				
+			case '+':
+				memory[dc]++
+				
+			case '-':
+				memory[dc]--
+				
+			case '.':
+				fmt.Printf("%c", memory[dc])
+				
+			case ',':
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print(">")
+				_, err := reader.Read(inp)
+				
+				if err != nil {
+					fmt.Println("ERROR: Could not read input")
+					return
+				}
+				
+				memory[dc] = inp[0]
+				
+			case '[':
+				if memory[dc] == 0 {
+					// search for matching closing
+					encounters := 1
+					 for {
+						pc++
+						
+						if pc >= len(program) || pc < 0 {
+							fmt.Println("ERROR: Program counter out-of-bounds")
+							return
+						}
+						
+						if program[pc] == '[' {
+							encounters++
+						} else if program[pc] == ']' {
+							encounters--
+							if encounters == 0 {
+								break
+							}
+						}
+					 }
+				}
+				
+			case ']':
+				if memory[dc] != 0 {
+					// search for matching opening
+					encounters := 1
+					 for {
+						pc--
+						
+						if pc >= len(program) || pc < 0 {
+							fmt.Println("ERROR: Program counter out-of-bounds")
+							return
+						}
+						
+						if program[pc] == ']' {
+							encounters++
+						} else if program[pc] == '[' {
+							encounters--
+							if encounters == 0 {
+								break
+							}
+						}
+					 }
+				}
+		}
+		
+		// Increment program counter
+		pc++
+	}
 }
 
 func main(){
-	fmt.Println("Starting interpreter...")
-	
-	// Initialize
-	//program = []byte("+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.")
-	program = []byte("++++++++[>+>++++<<-]>++>>+<[-[>>+<<-]+>>]>+[-<<<[->[+[-]+>++>>>-<<]<[<]>>++++++[<<+++++>>-]+<<++.[-]<<]>.>+[>>]>+]")
-	pc = 0
-	memory = make([]byte, 2048)
-	dc = 0
-	
-	inp = make([]byte, 1)
-	
-	for ; pc < len(program); {
-		step()
+
+	if len(os.Args) < 2 {
+		fmt.Println("ERROR: No program specified")
+		return
 	}
-	
+
+	load(os.Args[1])
+	run()
 }
